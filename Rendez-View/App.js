330fef855407289b5db1406f42b1819ce7914a85
Denzel Fynn <121134177+Shintogi@users.noreply.github.com>
2024-10-12 23:03:30 -0500

    CLean slate 1

--- Rendez-View/App.js
+++ Rendez-View/App.js
@@ -1,9 +1,30 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+let initialRegion = {
+  latitude: 37.7749,   // San Francisco
+  longitude: -122.4194,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
 
 export default function App() {
+  const [region, setRegion] = useState(initialRegion);
+
+  const onRegionChange = (region) => {
+    console.log('Region changed:', region);
+    setRegion(region); // Update the state with the new region
+  };
+
   return (
     <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={initialRegion} // Set the initial region
+        onRegionChange={onRegionChange} // Detect region change
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,5 +37,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    height: '80%', // Adjust size as needed
+    width: '100%',
   },
 });
